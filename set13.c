671fe5a3e782dae7819a3577aee24d1ba6831578
abhishek4864 <34052313+abhishek4864@users.noreply.github.com>
2018-01-28 10:47:31 -0500

    vowel or consonent

--- set13.c
+++ set13.c
@@ -6,13 +6,12 @@ void main()
 
     printf("Enter an alphabet: ");
     scanf("%c",&c);
-
-    
+	if((c>=65 && c<=90)||(c>=65&&c<=90))
+	{
     Vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
     if (Vowel)
         printf("%c is a vowel.", c);
     else
         printf("%c is a consonant.", c);
-    
+    }
 }
