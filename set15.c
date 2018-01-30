59cd9deeccc0e9ab15443e04361e735546d8f8a9
abhishek4864 <34052313+abhishek4864@users.noreply.github.com>
2018-01-30 18:24:31 -0500

    greater between three number

--- set15.c
+++ set15.c
@@ -2,14 +2,24 @@
 void main()
 {
 	int a,c,b;
-	printf("Enter the three values");
+	printf("Enter the three values\n");
 	scanf("%d%d%d",&a,&b,&c);
-	if(a>b&&a>c)
+	if(a>=b&&a>=c)
 	{
-		printf("a is greater");
+		if(a==b)
+			printf("a and b are graeter\n");
+		else if(a==c)
+			printf("a and c are greater\n");
+		else
+			printf("a is greater\n");
 	}
-	else if(b>c)
-		printf("b s greater");
+	else if(b>=c)
+	{
+		if(b==c)
+			printf("b and c are greater\n");
+		else
+			printf("b is greater\n");
+	}
 	else
-		printf("c is greater");
+		printf("c is greater\n");
 }
